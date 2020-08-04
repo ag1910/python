e3e2445bd8b370247611e9c7dd150ffd1cf104a4
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-08-04 11:58:30 +0300

    daha iyi hesapmakinesi

--- index.py
+++ index.py
@@ -1,12 +1,17 @@
-
-x = int(input("bir değer giriniz "))
-y = int(input("bir değer giriniz "))
-a = input("bir işlem giriniz ")
-if a == "+":
-    print("toplamları : ",x+y)
-if a == "-":
-    print("farkları : ",x-y)
-if a == "*":
-    print("çarpımları : ",x*y)
-if a == "/":
-    print("bölümleri : ",x/y)
+o = input("Yapmak istediğiniz şeyi girin(hesap makinesi,alan hesaplama) :")
+if o == "hesap makinesi":
+    x = int(input("bir değer giriniz "))
+    y = int(input("bir değer giriniz "))
+    a = input("bir işlem giriniz ")
+    if a == "+":
+        print("toplamları : ",x+y)
+    if a == "-":
+        print("farkları : ",x-y)
+    if a == "*":
+        print("çarpımları : ",x*y)
+    if a == "/":
+        print("bölümleri : ",x/y)
+if o == "alan hesaplama":
+    xa = int(input("bir kenar giriniz "))
+    ya = int(input("bir kenar giriniz "))
+    print("alanı : ",xa*ya)
