a94d4ffd266e91c13506d2b7399758498af60531
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-08-04 11:50:59 +0300

    hesap makinesi

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
+
 x = int(input("bir değer giriniz "))
 y = int(input("bir değer giriniz "))
-print("toplamları : ",x+y)
-print("farkları : ",x-y)
-print("çarpımları : ",x*y)
-print("bölümleri : ",x/y)
+a = input("bir işlem giriniz ")
+if a == "+":
+    print("toplamları : ",x+y)
+if a == "-":
+    print("farkları : ",x-y)
+if a == "*":
+    print("çarpımları : ",x*y)
+if a == "/":
+    print("bölümleri : ",x/y)
