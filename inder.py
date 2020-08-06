5a5100dac1540e5ed621b79fb5c215c910354e61
ag1910 <58875503+ag1910@users.noreply.github.com>
2020-08-06 17:22:40 +0300

    random

---
+++ inder.py
@@ -0,0 +1,10 @@
+ab = input("Bir aday giriniz ")
+ba = input("Başka bir aday giriniz ")
+cb = input("Başka bir aday giriniz ")
+dc = input("Başka bir aday giriniz ")
+ed = input("Başka bir aday giriniz ")
+
+import random
+
+fo = [ab, ba, cb, dc, ed]
+print(random.choice(fo))
